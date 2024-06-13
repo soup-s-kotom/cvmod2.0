608ff325d4c5eb9e4610ba8cfc070f7ab7b5f713
soupSKotom <fokin2317@gmail.com>
2024-06-13 11:40:20 +0600

    first version, empty mod, big plans

---
+++ src/main/java/net/cvcreators/calmoverse/CalmOVerseClient.java
@@ -0,0 +1,10 @@
+package net.cvcreators.calmoverse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CalmOVerseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cvcreators/calmoverse/CalmOVerseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cvcreators.calmoverse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CalmOVerseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
